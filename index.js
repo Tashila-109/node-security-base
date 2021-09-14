29ff46dca38a72036f4345c45f96212a29b3cac8
Tashila <tashila.dev@gmail.com>
2021-09-14 18:48:28 +0400

    Added in rate limiter

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import bodyParser from 'body-parser';
+import RateLimit from 'express-rate-limit';
 import helmet from 'helmet';
 
 import routes from './src/routes/crmRoutes';
@@ -10,6 +11,13 @@ const PORT = 3000;
 
 //helmet setup
 app.use(helmet());
+
+// Rate Limit setup
+const limiter = new RateLimit({
+    windowMs: 15*60*1000, // 15 minutes
+    max: 100, // imit of number of request per IP
+    delayMs: 0, // disabel delays
+})
 
 // mongoose connection
 mongoose.Promise = global.Promise;
